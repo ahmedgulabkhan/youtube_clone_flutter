5e0ae3a9355eec936f6b3ac72c2587c9c2bb15c0
demoncommand97 <demoncommand97@gmail.com>
2020-08-17 00:11:03 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/youtube_clone_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.youtube_clone_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone_flutter/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Youtube Clone',
+      home: HomePage(),
+    );
+  }
+}
